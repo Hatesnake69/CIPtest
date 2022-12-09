d1ea9d3695cbebcd3001f4b38746a0d0e292316e
Hatesnake69 <vladimir@itsy.dev>
2022-12-09 15:44:39 +0300

    add models

--- models/statistic.py
+++ models/statistic.py
@@ -1,7 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, Date, Numeric
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, Date, Float
 
 from db.engine import Base
 
@@ -13,14 +12,6 @@ class Statistic(Base):
     created_at = Column(Date, default=datetime.datetime.now())
     views = Column(Integer, default=0)
     clicks = Column(Integer, default=0)
-    cost = Column(Numeric(scale=2), default=0)
-    cost_per_click = Column(Numeric(scale=2), default=0)
-    cost_per_view = Column(Numeric(scale=2), default=0)
-
-
-class CreateStatisticCommand(BaseModel):
-    views: int = 0
-    clicks: int = 0
-    cost: float = 0
-    cost_per_click: float = 0
-    cost_per_view: float = 0
+    cost = Column(Float(precision=2), default=0)
+    cost_per_click = Column(Float(precision=2), default=0)
+    cost_per_view = Column(Float(precision=2), default=0)
